ce8943fbc13ae55316bcbb7677804c4dda0c467a
KunAce <wxkace@gmail.com>
2020-04-23 20:09:40 -0700

    Added some basic configurations for tomcat

---
+++ src/main/java/com/example/boot/App.java
@@ -0,0 +1,19 @@
+package com.example.boot;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan
+//@SpringBootApplication can replace @EnableAutoConfiguration and @ComponentScan
+public class App {
+    public static void main(String[] args) {
+//        SpringApplication.run(App.class, args); //Default method
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(App.class);
+        builder.bannerMode(Banner.Mode.OFF).run(args);
+    }
+}
--- src/main/java/com/example/boot/mycontroller/HelloController.java
+++ src/main/java/com/example/boot/mycontroller/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.boot.mycontroller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "Hello world!";
     }
